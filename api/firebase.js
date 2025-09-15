8a3e9702c91c8e4f84ed902230135363a7fea0ff
mouha-byte <mouhanedmliki6@outlook.com>
2025-09-15 08:56:40 +0100

    change

---
+++ api/firebase.js
@@ -0,0 +1,17 @@
+// api/firebase-config.js (backend)
+import express from 'express';
+const router = express.Router();
+
+router.get('/config', (req, res) => {
+  res.json({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  });
+});
+
+export default router;
