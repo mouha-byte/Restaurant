c1850c729d0d9bb4dd88c414f4c2e2f8647b08c0
mouha-byte <mouhanedmliki6@outlook.com>
2025-09-17 08:16:35 +0100

    payemnet ok

--- api/config.js
+++ api/config.js
@@ -1,16 +1,16 @@
-// api/firebase-config.js (backend)f
-import express from 'express';
+// api/config.js
+import express from "express";
 const router = express.Router();
 
-router.get('/config', (req, res) => {
+router.get("/config", (req, res) => {
   res.json({
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
     projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
+    storageBucket: `${process.env.FIREBASE_PROJECT_ID}.firebasestorage.com`,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
   });
 });
 
